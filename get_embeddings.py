50dc497012ebfb7ea1d6cab4bd70a51666cf98e9
dron <dr_dronych@mail.ru>
2019-07-11 22:03:20 +0600

    removed debugging code

--- get_embeddings.py
+++ get_embeddings.py
@@ -17,22 +17,14 @@ def main(images_dir, model_dir):
 		images = load_images(images_dir, [160, 160])
 		images = images.batch(batch_size=1, drop_remainder=True)		
 		iterator = images.make_one_shot_iterator()
-		# next_element = iterator.get_next()
-		# image_batch = tf.train.batch_join(next_element, batch_size=128,
-  #                       enqueue_many=False, allow_smaller_final_batch=True)
-		
-		# print(next_element.shape)
-		# print(next_element)
 		
 		init = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
 		sess.run(init)
 
 		embeddings_layer = tf.get_default_graph().get_tensor_by_name('embeddings:0')
-		# print(embeddings_layer.shape)
 		img_placeholder = tf.get_default_graph().get_tensor_by_name('input:0')
 		phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
-		# print(phase_train_placeholder.__class__)
-		# res = img_placeholder(next_element)
+
 		embeddings_array = None
 
 		while True:
@@ -41,17 +33,8 @@ def main(images_dir, model_dir):
 				next_element = sess.run(next_element)
 				embeddings = sess.run(embeddings_layer, feed_dict={img_placeholder: next_element,
 										phase_train_placeholder: False})
-				# print(embeddings.shape)
-				# print(embeddings)
-				# print(embeddings)
-				# print('*' * 40)
-				# print(embeddings_array)
-				# print('*' * 40)
 
 				embeddings_array = np.concatenate((embeddings_array, embeddings)) if embeddings_array is not None else embeddings
-				# print('Emb array')
-				# print(embeddings_array)
-				
 
 			except tf.errors.OutOfRangeError:
 				break
@@ -68,7 +51,6 @@ def load_model(model_dir):
 		if os.path.splitext(fname)[1] == '.pb':
 			model_file = os.path.join(MODEL_DIR, fname)
 			break
-	# print(model_file)
 
 	assert model_file is not None
 
